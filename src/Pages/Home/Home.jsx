c1be7b71fea8a8ae2f95f07fb9e1401b0d5e1327
sabir689 <sahariarpub@gmail.com>
2023-11-08 18:05:32 +0600

    updated

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -68,11 +68,22 @@ const Home = () => {
                     </div>
                 </div>
             </div>
-            <div className="stats shadow-2xl border-2 p-8 ml-60 mt-10 mb-10">
-
+            <div className="stats shadow-2xl border-2 p-8 ml-4 md:ml-60 mt-10 mb-10 grid grid-cols-1 md:grid-cols-3 gap-4">
                 <div className="stat">
                     <div className="stat-figure text-primary">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            className="inline-block w-8 h-8 stroke-current"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                            ></path>
+                        </svg>
                     </div>
                     <div className="stat-title">Total Likes</div>
                     <div className="stat-value text-primary">25.6K</div>
@@ -81,7 +92,19 @@ const Home = () => {
 
                 <div className="stat">
                     <div className="stat-figure text-secondary">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            className="inline-block w-8 h-8 stroke-current"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M13 10V3L4 14h7v7l9-11h-7z"
+                            ></path>
+                        </svg>
                     </div>
                     <div className="stat-title">Page Views</div>
                     <div className="stat-value text-secondary">2.6M</div>
@@ -100,9 +123,9 @@ const Home = () => {
                     <div className="stat-title">Tasks done</div>
                     <div className="stat-desc text-secondary">31 tasks remaining</div>
                 </div>
-
             </div>
-            
+
+
         </div>
     );
 };
