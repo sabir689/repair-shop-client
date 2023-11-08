d982b0b00ac467ab91cbba4665ae1a19068ef50a
sabir689 <sahariarpub@gmail.com>
2023-11-08 14:19:00 +0600

    section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -9,10 +9,21 @@ const Home = () => {
         <div>
             <Banner></Banner>
             <Services Home={true}></Services>
-         <Link to={"allServices"}>
-         <div className="justify-center flex mb-10">
-          <button className="btn btn-primary">All Services</button>
-          </div></Link>
+            <Link to={"allServices"}>
+                <div className="justify-center flex mb-10">
+                    <button className="btn btn-primary">All Services</button>
+                </div></Link>
+                <div className="hero min-h-screen bg-base-200">
+    <div className="hero-content flex-col lg:flex-row">
+        <img src="https://i.ibb.co/9bHhF78/ser6.jpg" className="max-w-sm rounded-lg shadow-2xl" />
+        <div>
+            <h1 className="text-5xl font-bold">Professional Repair Services</h1>
+            <p className="py-6">We provide top-notch repair services for all your needs. Trust us for quality repairs and quick solutions to your problems.</p>
+            <button className="btn btn-primary">Get Started</button>
+        </div>
+    </div>
+</div>
+
         </div>
     );
 };
